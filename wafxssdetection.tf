122fcbe3150334067a13738065627df7fa59f972
Glen Ogilvie <glen.ogilvie@oss.co.nz>
2019-05-31 11:28:13 +1200

    correction

--- wafxssdetection.tf
+++ wafxssdetection.tf
@@ -20,49 +20,42 @@ resource "aws_wafregional_xss_match_set" "WAFXssDetection" {
     text_transformation = "URL_DECODE"
     field_to_match {
       type = "QUERY_STRING"
-      data = "none"
     }
   }
   xss_match_tuple {
     text_transformation = "HTML_ENTITY_DECODE"
     field_to_match {
       type = "QUERY_STRING"
-      data = "none"
     }
   }
   xss_match_tuple {
     text_transformation = "URL_DECODE"
     field_to_match {
       type = "BODY"
-      data = "none"
     }
   }
   xss_match_tuple {
     text_transformation = "HTML_ENTITY_DECODE"
     field_to_match {
       type = "BODY"
-      data = "none"
     }
   }
   xss_match_tuple {
     text_transformation = "URL_DECODE"
     field_to_match {
       type = "URI"
-      data = "none"
     }
   }
   xss_match_tuple {
     text_transformation = "HTML_ENTITY_DECODE"
     field_to_match {
       type = "URI"
-      data = "none"
     }
   }
   xss_match_tuple {
     text_transformation = "HTML_ENTITY_DECODE"
     field_to_match {
       type = "URI"
-      data = "none"
     }
   }
   xss_match_tuple {
